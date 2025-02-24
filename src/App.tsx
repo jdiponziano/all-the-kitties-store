a75f7a0af3639de245455091d232cc7e99fa0985
jdiponziano <jdiponziano@gmail.com>
2025-02-24 16:28:34 -0500

    refactor(global): :truck: move category data to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { categoryCardData } from "./data/categories";
+
 const App = () => {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="container">
+      <ul>
+        {categoryCardData.map((card) => (
+          <li key={`category-card-${card.id}`}>
+            <a className="category-card" href={card.linkUrl}>
+              <img alt="" src={card.imageUrl} />
+              <div className="category-card__content">
+                <h3 className="category-card__title">{card.title}</h3>
+                <p className="category-card__description">Shop Now</p>
+              </div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/data/categories.ts
@@ -0,0 +1,39 @@
+type CategoryCardProps = {
+  id: number;
+  title: string;
+  imageUrl: string;
+  linkUrl: string;
+};
+
+export const categoryCardData: CategoryCardProps[] = [
+  {
+    id: 1,
+    title: "Hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    linkUrl: "shop/hats",
+  },
+  {
+    id: 2,
+    title: "Jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    linkUrl: "shop/jackets",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    linkUrl: "shop/sneakers",
+  },
+  {
+    id: 4,
+    title: "Womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    linkUrl: "shop/womens",
+  },
+  {
+    id: 5,
+    title: "Mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/mens.png",
+    linkUrl: "shop/mens",
+  },
+];

9b75508e85c3df2d56bb6c382ebb466f87438b58
jdiponziano <jdiponziano@gmail.com>
2025-02-28 13:42:29 -0500

    feat: :lipstick: create input form element component

---
+++ src/components/form-elements/Input.tsx
@@ -0,0 +1,23 @@
+import { FC, InputHTMLAttributes } from "react";
+import "./input.scss";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  id: string;
+}
+
+const Input: FC<InputProps> = ({ label, id, ...otherProps }) => {
+  return (
+    <div className="form-group">
+      <label
+        className={`${!!otherProps.value ? "shrink" : ""} form-label`}
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <input className="form-input" id={id} {...otherProps} />
+    </div>
+  );
+};
+
+export default Input;
